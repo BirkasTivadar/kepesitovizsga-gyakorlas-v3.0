a3c91305124b0171b566869a735d21016549a498
BirkasTivadar <birkastivadar@gmail.com>
2021-04-25 10:21:53 +0200

    Initial commit

---
+++ src/test/java/hu/nive/ujratervezes/kepesitovizsga/uppercase/UpperCaseLettersTest.java
@@ -0,0 +1,12 @@
+package hu.nive.ujratervezes.kepesitovizsga.uppercase;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UpperCaseLettersTest {
+
+//    @Test
+//    public void testGetNumberOfUpperCase() {
+//        Assertions.assertEquals(8, new UpperCaseLetters().getNumberOfUpperCase("characters.txt"));
+//    }
+
+}
