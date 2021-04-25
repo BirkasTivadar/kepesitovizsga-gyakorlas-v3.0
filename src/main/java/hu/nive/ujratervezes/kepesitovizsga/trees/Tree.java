b74c1bf484b70cccf160888078954a3590311f9b
BirkasTivadar <birkastivadar@gmail.com>
2021-04-25 11:38:37 +0200

    Trees kész

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/trees/Tree.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/trees/Tree.java
@@ -2,7 +2,7 @@ package hu.nive.ujratervezes.kepesitovizsga.trees;
 
 public abstract class Tree {
 
-    public static final int BIRD_NEST_PER_LEAVES = 200;
+    static final int BIRD_NEST_PER_LEAVES = 200;
 
     private int leaves;
 
